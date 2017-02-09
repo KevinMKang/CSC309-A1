eee90124de0563ee37efe7b04f79052451d2ae45
KevinMKang <kevinmkang97@gmail.com>
2017-02-09 18:28:13 -0500

    looks ok now, need to comment and format post boxes

--- profile.js
+++ profile.js
@@ -10,27 +10,33 @@ $(function(){
         if (text != undefined) {
             post = makeNewPost(text, false);
             new_post = $('.post_feed').append($(post)) 
-        }
-		
+        }		
     });
 });
 
 function addLike(id){
 	var likes = parseInt(document.getElementById('like'+id).innerHTML);
-	document.getElementById('like'+id).innerHTML=likes+1;
+	document.getElementById('like'+id).innerHTML=likes+1 + ' Likes';
 }
 
 function makeNewPost(text,image){
 	id+=1;
-	content = image ? '<img src="' + text + ' "/>' : text;
+	content = image ? '<img class="imgPost" src="' + text + ' "/>' : text;
 	return (
-	'<div class="post test--post"> \
-		<img src="assets/avatar.png"/>\
-		'+ document.getElementById('profile_name').innerHTML + (new Date()).toString()+ '\
-		Likes: <p id="like'+id+'">0</p>\
-		<p class="post_content test--post_content">' + content + '</p>\
-		<button type="submit" id="'+id+'" class="like_button" onClick="addLike(this.id)" >Like this Post</button>\
-		</div>'
+	'<div class="post test--post">\
+		<div class="post_head">\
+			<img class="poster_avatar" src="assets/avatar.png"/> \
+			<div class="poster_info">\
+				<p class="poster_name"><b>'+ document.getElementById('profile_name').innerHTML +'</b></p>\
+				<i>' +(new Date()).toLocaleTimeString()+ ' ' +(new Date()).toDateString() + '</i>\
+				<div class="like_bar"> \
+					<p id="like'+id+'" class="like_count test--like_count">0 Likes</p>\
+					<button type="submit" id="'+id+'" class="like_button test--like_button" onClick="addLike(this.id)" >Like this Post</button>\
+				</div>\
+			</div>\
+		</div>\
+	<p class="post_content test--post_content">' + content + '</p>\
+	</div>'
 	);
 }
 
