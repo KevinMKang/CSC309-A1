6ef48394c3de1ba44ad1ab5517aa92cbb7502bd3
KevinMKang <kevinmkang97@gmail.com>
2017-02-10 17:08:08 -0500

    Code commented and modularized, submitting this

--- profile.js
+++ profile.js
@@ -1,41 +1,72 @@
+
+//This variable is used to keep track of the like button/like count pairs, to give them unique ids
 var id = 0;
+
+//An array created with accepted img types to check whether a given URL is an image or not.
 var imgTypes = ["png", "jpg", "gif", "bmp"];
+
+//Function that handles the opening of the menu/navbar at the top of the page, when it is collapsed in mobile mode
 function openMenu(){
 	document.getElementById('menu').style.display =  document.getElementById('menu').style.display == 'block' ? 'none' : 'block';
 }
 
-
+//To open the input text box
 function openTextBox(){
 	document.getElementById('textbox').style.display =  document.getElementById('textbox').style.display == 'block' ? 'none' : 'block';
-	//document.getElementById('post_box').style.display =  document.getElementById('post_box').style.display == setting ? 'none' : setting;
 }
 
+//To open the URL input box
 function openPhotoBox(){
 	document.getElementById('photobox').style.display =  document.getElementById('photobox').style.display == 'block' ? 'none' : 'block';
-	//document.getElementById('post_box').style.display =  document.getElementById('post_box').style.display == setting ? 'none' : setting;
 }
 
+//Creates a text post from the text input field and prepends it to the post feed
 $(function(){
     $('.input_button').on('click', function(){
         text = $('.input_text').val();
         if (text != undefined && text!='') {
             post = makeNewPost(text, false);
-            new_post = $('.post_feed').append($(post));
+            new_post = $('.post_feed').prepend($(post));
 			$('.input_text').val('');
 			openTextBox();
         }		
     });
 });
 
+//Similar to the function above, this creates an image post from the URL input field and prepends it to the post feed
+$(function(){
+    $('.photo_button').on('click', function(){
+        text = $('.photo_url').val();
+		
+		//Check whether it's an image or not by seeing the file extension
+        if (text != undefined && text!='' && imgTypes.indexOf(text.substring(text.length-3,text.length))!=-1 ){
+            post = makeNewPost(text,true);
+            new_post = $('.post_feed').prepend($(post));
+			$('.photo_url').val('');
+			openPhotoBox();
+        }
+		
+    });
+});
+
+//Adds a like to the like counter on the id of the post, each like counter has a unique id
 function addLike(id){
 	var likes = parseInt(document.getElementById('like'+id).innerHTML);
 	document.getElementById('like'+id).innerHTML=likes+1 + ' Likes';
 }
 
+/*Creates a new post in correct HTML formatting
+* It has two inputs- the actual text content, and a boolean image whether it's an image or not. 
+* It returns the correctly formatted post
+*/
 function makeNewPost(text,image){
 	id+=1;
-	content = image ? '<img class="imgPost" src="' + text + ' "/>' : text;
+	content = image ? '<img class="imgPost" src="' + text + ' "/>' : text; //Create an image from the input text
 	
+	/*Every post has a header - containing the poster avatar, info, and post date. As well, there's a sub like bar containing 
+	* a like button a like count containing a unique id
+	* After that, there's the actual post content containing the content, either text or an image URL
+	*/
 	return (
 	'<div class="post test--post">\
 		<div class="post_head">\
@@ -53,19 +84,5 @@ function makeNewPost(text,image){
 	</div>'
 	);
 }
-
-$(function(){
-    $('.photo_button').on('click', function(){
-        text = $('.photo_url').val();
-        if (text != undefined && text!='' && imgTypes.indexOf(text.substring(text.length-3,text.length))!=-1 ){
-            post = makeNewPost(text,true);
-            new_post = $('.post_feed').append($(post));
-			$('.photo_url').val('');
-			openPhotoBox();
-        }
-		
-    });
-});
-
 
 
